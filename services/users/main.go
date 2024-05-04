ec8c005e8e170ebcda8e05e650bc6c1dd348ec61
Akarys <prof.aka777@gmail.com>
2024-05-04 07:01:24 +0500

    da

--- services/users/main.go
+++ services/users/main.go
@@ -1,1 +1,16 @@
-package users
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've reached the Users Service")
+	})
+
+	log.Println("Users service listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
